1dd3d92861eb0219641b6277d8653a89a20ab42f
Chris Choy <chrischoy208@gmail.com>
2017-07-06 13:20:38 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,9 +4,6 @@ from build import mathutils
 
 class BroadcastAccum(Function):
   """Accumulate x += y using broadcasting sum.
-
-  By default, pytorch does not support broadcasting sum. Instead, it expand the
-  tensor to do the broadcasting sum, which is inefficient use of memory.
   """
   def forward(self, x, y):
     return mathutils.broadcast_sum(x, y)
